fec929a86af8fea5438b6d9380cb5322c0d94df8
Rosnel14 <leyvarosnel@gmail.com>
2020-03-30 19:48:15 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ import acm.program.*;
 public class HelloWorld extends ConsoleProgram{
 	public void run() {
 		println("Hello World");
+		println("Rosnel says Hi.") //Used Atom to write this, could not use Ecclipse 
 	}
 }
